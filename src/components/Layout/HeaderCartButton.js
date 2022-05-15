69926a8e74426151bfce541d2bc7e683a48a9cd9
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-05-16 00:01:08 +0530

    fix typos

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,44 +1,45 @@
-import React, { useState, useContext, useEffect } from 'react'
-import classes from "./HeaderCartButton.module.css"
-import CartIcon from '../Cart/CartIcon'
-import CartContext from '../../store/cart-context'
+import React, { useState, useContext, useEffect } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = ({onClick}) => {
+const HeaderCartButton = ({ onClick }) => {
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const cartCtx = useContext(CartContext);
-  
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
-    return curNumber + item.amount;
-  }, 0)
-
   const { items } = cartCtx;
 
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   useEffect(() => {
-    if(items.length === 0){
+    if (items.length === 0) {
       return;
     }
 
-    setBtnIsHighlighted(true)
-    const timer = setTimeout(()=>{
-      setBtnIsHighlighted(false)
-    }, 300)
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
 
     return () => {
-      clearInterval(timer)
-    }
-  }, [items])
+      clearInterval(timer);
+    };
+  }, [items]);
 
-  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
   return (
     <button className={btnClasses} onClick={onClick}>
-        <span className={classes.icon}>
-            <CartIcon/>
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>{numberOfCartItems}</span>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-  )
-}
+  );
+};
 
-export default HeaderCartButton
+export default HeaderCartButton;
